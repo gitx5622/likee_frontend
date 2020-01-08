ef3819de6b2e144a3580f6887fbb295cda4abc48
Anthony Gitau <gitau.ag@gmail.com>
2020-01-08 18:57:39 +0300

    backend integrated

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@ let API_ROUTE;
 
 process.env.NODE_ENV === 'development'
     ? API_ROUTE = 'http://127.0.0.1:9000'
-    : API_ROUTE = 'https://likee.co.ke';
+    : API_ROUTE = 'http://167.71.137.103:9000';
 
 export default API_ROUTE
