04e00d88e7fdb3e3ee01c0f7a9da8addc4e97890
Anthony Gitau <gitau.ag@gmail.com>
2020-01-10 16:53:08 +0300

    final url change

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@ let API_ROUTE;
 
 process.env.NODE_ENV === 'development'
     ? API_ROUTE = 'http://127.0.0.1:9000'
-    : API_ROUTE = 'http://178.62.77.252:9000';
+    : API_ROUTE = 'https://jobhub.co.ke';
 
 export default API_ROUTE
