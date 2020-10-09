df9e01a5ceb17eefe2d0a4dc3aa24a67927a60ca
gitx5622 <ggitau5622@gmail.com>
2020-10-09 18:16:02 +0300

    constants

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 let API_ROUTE;
 
 if (process.env.NODE_ENV === "production") {
-  API_ROUTE = "https://admin.likee.co.ke";
+  API_ROUTE = "http://165.232.34.66:9000";
 } else {
   API_ROUTE = "http://127.0.0.1:9000";
 }
