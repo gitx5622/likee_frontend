81b6eb51ccc319005efe63574ca41cc7c9ee35b5
gitx5622 <ggitau5622@gmail.com>
2020-10-09 20:18:43 +0300

    constants

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 let API_ROUTE;
 
 if (process.env.NODE_ENV === "production") {
-  API_ROUTE = "http://165.232.34.66:9000";
+  API_ROUTE = "https://jobhub.co.ke";
 } else {
   API_ROUTE = "http://127.0.0.1:9000";
 }
